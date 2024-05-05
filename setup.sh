bed164b7be915e7498dfe05f94ea76ed4be0bc3e
Mehmet Ali Baykara <mab.baykara@gmail.com>
2024-05-05 22:18:18 +0200

    fix repo path

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ if [ -d ~/.local-terminal ]; then
     source ~/.local-terminal/.custom_bash
 else
     echo "No .local-terminal directory found, cloning it from GitHub..."
-    git clone https://github.com/mbaykara/local-terminal.git ~/.local-terminal
+    git clone https://github.com/mbaykara/.local-terminal.git ~/.local-terminal
     source ~/.local-terminal/.custom_bash
     echo "Copying git config..."
     cp ~/.local-terminal/.gitconfig ~/.gitconfig
